32628b3c1015105e9ecdea874d55751c0e63c253
Sta3s <stass.bernatovics@gmail.com>
2024-02-28 12:19:08 +0200

    Pd

--- main.py
+++ main.py
@@ -1,12 +1,82 @@
-# Dokumentācija
-# Mainīgie - https://www.w3schools.com/python/python_variables.asp
-# Operācijas ar mainīgiem - https://www.w3schools.com/python/python_operators.asp
-# Mainīgo drukāšana - https://www.w3schools.com/python/python_variables_output.asp
-# Nosacījumi, zarošana, if ... else - https://www.w3schools.com/python/python_conditions.asp
-# For cikls - https://www.w3schools.com/python/python_for_loops.asp
-# Nejauša skaitļa generēšana - https://www.w3schools.com/python/ref_random_randint.asp
-# Github Fork (repozitorija kopija) - https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/fork-a-repo
-# Saraksti - https://www.w3schools.com/python/python_lists.asp
-# Vārdnīcas - https://www.w3schools.com/python/python_dictionaries.asp
-# Klonēt repozitoriju - hhttps://code.visualstudio.com/docs/sourcecontrol/intro-to-git
+import json
 
+
+watchlist = []
+# example
+#watch_list = [["gang", 6],["god", 10,"watched"],["fff", 9], ["how", 2]]
+
+
+# 1. to add a film
+# film = input()
+# movie = film.split(" ")
+# !!! checking if everything is ok with count of chars
+# if len(movie[0]) < 121 and len(movie[0]) > 1 and int(movie[1]) < 11 and int(movie[1]) > 0 :
+# !!! checking if every value is in right type
+#     try:
+#         watch_list.append([str(movie[0]),int(movie[1])])
+#     except:
+#         print("smth got wrong")
+#     print(watch_list)
+# else:
+#     print("smth got wrong")
+
+
+# 2. to delete a film
+# film_to_delete = input()
+# x = 0
+# !!! searching film to delete in watch_list
+# for i in watch_list:
+# !!! if there is that film so we delete it
+#     if film_to_delete == i[0]:
+#         del watch_list[x]
+#     else:
+#         print("")
+#     x +=1
+# print(watch_list)
+
+# 3. to mark a film as watched
+# film_is_watched = input()
+# !!! scrolling whole watch_list to find a film we watched
+# for i in watch_list:
+# !!! if there is we mark it as watched
+#     if film_is_watched in i:
+#         i.append("watched")
+# print(watch_list)
+
+# 4. to split(filter) watched and non watched films
+# watched_films = []
+# non_watched_films = []
+# !!! scrolling the whole watch_list
+# for i in watch_list:
+# !!! if there is some extra value(watched) we add this film in watched list
+#     if len(i) == 3:
+#         watched_films.append(i)
+#     else:
+#         non_watched_films.append(i)
+# print(watched_films)
+# print(non_watched_films)
+
+# 5. to get top 10 films by marks
+# returning only marks
+# def myFunc(e):
+#     return e[1]
+# watch_list.sort(key=myFunc,reverse=True)
+# !!! printing only first 10 films from sorted list
+# for i in range(10):
+#     print(watch_list[i])
+
+# 6. to empty a watch_list
+# del watch_list[:]
+# print(watch_list)
+
+# 7. to search a film by part of name
+# !!! checking that our string is string
+# try:
+#     part = str(input())
+# except:
+#     print("smth got wrong")
+# !!! scrolling list
+# for i in watch_list:
+# !!! it will print these films if there would be just a part of whole name
+#     if part in i[0]:
+#         print(i)
